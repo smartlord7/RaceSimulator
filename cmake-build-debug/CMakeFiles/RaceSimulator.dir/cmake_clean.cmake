777f1d7e0620368ea443fae547d1ca9b3076a1f8
Sancho Amaral Simões <ssimoes@ipn.pt>
2021-04-01 14:47:47 +0100

    MAIN: Create C project in CLion.

---
+++ cmake-build-debug/CMakeFiles/RaceSimulator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RaceSimulator.dir/main.c.o"
+  "RaceSimulator.exe"
+  "RaceSimulator.pdb"
+  "libRaceSimulator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/RaceSimulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
