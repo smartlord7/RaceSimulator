bf095a075f69a0ea3fb42cba433710aede7a4505
jfgartur@hotmail.com <Amigos2019217853>
2021-04-02 14:21:48 +0100

    RACESIMULATOR: Add process creation functionalities
    RACEMANAGER: Add team manager process creation functionality
    ERRORHANDLER: Add some types of error
    TEAMMANAGER: Start thread creation functionality

--- malfunction_manager.c
+++ malfunction_manager.c
@@ -1,1 +1,11 @@
 #include "global.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "util/error_handler.h"
+
+#define MALFUNCTION_MANAGER "MALFUNCTION MANAGER"
+
+int malfunction_manager(){
+    throw_error(ERROR_NOT_IMPLEMENTED, MALFUNCTION_MANAGER);
+}
--- team_manager.c
+++ team_manager.c
@@ -1,1 +1,30 @@
 #include "global.h"
+#include "structs/shared_memory.h"
+#include <stdlib.h>
+#include <pthread.h>
+#include "util/error_handler.h"
+
+#define CAR_THREAD "CAR THREAD"
+
+int start_data();
+void * create_car(void * car_info){
+    race_car car = *((race_car *) car_info);
+    throw_error(ERROR_NOT_IMPLEMENTED, CAR_THREAD);
+}
+
+
+int max_num_of_cars, num_of_cars, * cars_id;
+pthread_t * my_cars;
+
+
+int team_manager(){
+
+}
+
+int start_data(){
+    max_num_of_cars = mem_struct->cfg->max_cars_per_team;
+    cars_id = malloc(max_num_of_cars * sizeof(int));
+
+
+}
+
