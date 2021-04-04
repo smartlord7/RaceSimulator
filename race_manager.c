08b91963ce607b9ef5f9c759e1bbc52a78a7c88e
Sancho Amaral Simões <ssimoes@ipn.pt>
2021-04-04 21:38:05 +0100

    MAIN: Complete file headers.

--- race_manager.c
+++ race_manager.c
@@ -1,10 +1,11 @@
-/*
- * Authors:
- *  - Joao Filipe Guiomar Artur, 2019217853
- *  - Sancho Amaral Simoes, 2019217590
- *
- * Date of creation: 02/04/2021
- */
+/* Project RaceSimulator - LEI, University of Coimbra, 2nd year, 2nd semester - Operating Systems
+*
+* Authors:
+*  - Joao Filipe Guiomar Artur, 2019217853
+*  - Sancho Amaral Simoes, 2019217590
+*
+* Date of creation: 01/04/2021
+*/
 
 #include <stdlib.h>
 #include <unistd.h>
--- team_manager.h
+++ team_manager.h
@@ -1,10 +1,11 @@
-/*
- * Authors:
- *  - Joao Filipe Guiomar Artur, 2019217853
- *  - Sancho Amaral Simoes, 2019217590
- *
- * Date of creation: 02/04/2021
- */
+/* Project RaceSimulator - LEI, University of Coimbra, 2nd year, 2nd semester - Operating Systems
+*
+* Authors:
+*  - Joao Filipe Guiomar Artur, 2019217853
+*  - Sancho Amaral Simoes, 2019217590
+*
+* Date of creation: 01/04/2021
+*/
 
 #ifndef GLOBAL_HEADER_H_TEAM_MANAGER_H
 #define GLOBAL_HEADER_H_TEAM_MANAGER_H
