46ca0371f339e57e9f88f7c371c05cfe97b3acaf
jfgartur@hotmail.com <Amigos2019217853>
2021-05-05 23:51:54 +0100

    MAIN: Continue developing priority queue feature

--- priority_qeue.c
+++ priority_qeue.c
@@ -2,17 +2,20 @@
 // Created by jfgar on 22/04/2021.
 //
 
+#include <stdlib.h>
 #include "priority_qeue.h"
 
 #define PRIORITY_ORDER 2
+#define MAX_QUEUE_SIZE 100
 
-int safety_state_priority;
-int capacity;
-int queue[3];
+int safety_state_priority, queue_size;
+queue_element_t priority_queue[MAX_QUEUE_SIZE];
+queue_element_t root;
 
-void priority_queue_init(int queue_capacity, int fuel_tank_capacity){
-    capacity = queue_capacity;
+void priority_queue_init(int fuel_tank_capacity){
     safety_state_priority = PRIORITY_ORDER * fuel_tank_capacity;
+    bzeros(priority_queue, MAX_QUEUE_SIZE * sizeof(queue_element_t));
+    queue_size = 0;
 }
 
 int calculate_priority(race_car_t race_car){
@@ -28,7 +31,7 @@ int calculate_priority(race_car_t race_car){
 }
 
 struct queue_element_t create_element(race_car_t car, int priority){
-    queue_element element;
+    queue_element_t element;
 
     element.race_car = car;
     element.priority = priority;
@@ -36,13 +39,96 @@ struct queue_element_t create_element(race_car_t car, int priority){
     return element;
 }
 
-void insert_car_in_waiting_queue(race_car_t car){
-    queue_element race_car;
-    int priority;
+void update_root(){
+    root = priority_queue[0];
+}
 
-    priority = calculate_priority(car);
-    race_car = create_element(car, priority);
+void swap(queue_element_t * a, queue_element_t * b){
+    queue_element_t temp;
 
-
+    temp = *a;
+    *a = *b;
+    *b = temp;
 
 }
+
+int insert_element(race_car_t car) {
+    int i;
+    queue_element_t node;
+
+    node = create_element(car, calculate_priority(car));
+
+    i = 0;
+    while(i < MAX_QUEUE_SIZE){
+        if(priority_queue[i].priority == 0){
+            priority_queue[i] = node;
+            queue_size += 1;
+            break;
+        } else if(node.priority >= priority_queue[i].priority) {
+            i = 2 * i + 2;
+        } else if(node.priority < priority_queue[i].priority) {
+            i = 2 * i + 1;
+        }
+    }
+
+    if(i < MAX_QUEUE_SIZE) {
+        bring_up(i);
+    }
+
+    return EXIT_FAILURE;
+}
+
+void bring_up(int index) {
+    int i = index;
+
+    while(i != 0 && priority_queue[i].priority > priority_queue[(i - 1) / 2].priority ) {
+        swap(&priority_queue[i], &priority_queue[(i - 1) / 2]);
+        i = (i - 1) / 2;
+    }
+
+    update_root();  
+}
+
+void update_position(int index){
+    int i = index;
+
+    while(true) {
+        if(priority_queue[i].priority > priority_queue[2 * i + 2].priority) {
+            swap(&priority_queue[i], &priority_queue[2 * i + 2]);
+            i = 2 * i + 2;
+
+        } else if (priority_queue[i].priority < priority_queue[2 * i + 1].priority) {
+            swap(&priority_queue[i], &priority_queue[2 * i + 1]);
+            i = 2 * i + 1;
+
+        } else break;
+    }
+
+}
+
+race_car_t get_next_car() {
+    race_car_t car;
+
+    car = priority_queue[0].race_car;
+
+    if(queue_size > 1) {
+        swap(&priority_queue[0], &priority_queue[queue_size - 1]);
+
+        update_position(0);
+        update_root();
+    }
+
+    bzeros(priority_queue[queue_size - 1], sizeof(queue_element_t));
+    queue_size--;
+
+    return car;
+}
+
+/*
+int find_car(race_car_t car){
+    int i = 0;
+
+    while(i < MAX_QUEUE_SIZE) {
+
+    }
+}*/
--- priority_qeue.h
+++ priority_qeue.h
@@ -8,7 +8,7 @@
 #include "race_car_t.h"
 #include "race_box_t.h"
 
-typedef struct queue_element_t queue_element;
+typedef struct queue_element_t queue_element_t;
 
 struct queue_element_t {
     int priority;
@@ -17,6 +17,6 @@ struct queue_element_t {
 
 void insert_car_in_waiting_queue(race_car_t car);
 
-void priority_queue_init(int queue_capacity, int fuel_tank_capacity);
+void priority_queue_init(int fuel_tank_capacity);
 
 #endif //LOG_GENERATOR_C_PRIORITY_QEUE_H
