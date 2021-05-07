8c4d62fd1a4d1837a9aa4648dd12396de528799c
João <jfgartur@hotmail.com>
2021-05-07 22:41:29 +0100

    MAIN: Recovered race manager source

--- processes/race_manager/race_manager.h
+++ processes/race_manager/race_manager.h
@@ -1,1 +1,53 @@
 
+/** Project RaceSimulator - LEI, University of Coimbra, 2nd year, 2nd semester - Operating Systems
+*
+* @author
+*  - Joao Filipe Guiomar Artur, 2019217853
+*  - Sancho Amaral Simoes, 2019217590
+*
+* @date 01/04/2021
+*/
+
+#ifndef RACESIMULATOR_C_RACE_MANAGER_H
+#define RACESIMULATOR_C_RACE_MANAGER_H
+
+// region constants
+
+#define DELIM_1 ","
+#define DELIM_2 ":"
+
+#define RACE_MANAGER "RACE_MANAGER"
+#define TEAMS "TEAMS"
+#define BOXES "BOXES"
+#define ADDCAR "ADDCAR"
+#define START "START"
+#define CAR "CAR"
+#define TEAM "TEAM"
+#define SPEED "SPEED"
+#define CONSUMPTION "CONSUMPTION"
+#define RELIABILITY "RELIABILITY"
+#define N_PIPE_WATCHER "NAMED PIPE WATCHER"
+#define START_RACE "START_RACE!"
+
+#define RESULT_NOT_RECO 0
+#define RESULT_INVALID_COMMAND 1
+#define RESULT_NEW_CAR 2
+
+
+
+#define COMMAND_PREVIEW_SIZE 7
+
+// endregion constants
+
+// region public functions prototypes
+
+/**
+ * @def race_manager
+ * @brief Worker function executed when the race manager process is created.
+ *
+ */
+void race_manager();
+
+// endregion public functions prototypes
+
+#endif //RACESIMULATOR_C_RACE_MANAGER_H
