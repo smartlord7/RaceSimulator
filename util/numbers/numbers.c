45ee9d5414b352dd4bde5892072236bebf246fdd
João <jfgartur@hotmail.com>
2021-05-22 22:03:53 +0100

    STABLEv2.3: Add documentation

--- util/numbers/numbers.c
+++ util/numbers/numbers.c
@@ -1,7 +1,20 @@
+/** Project RaceSimulator - LEI, University of Coimbra, 2nd year, 2nd semester - Operating Systems
+*
+* @author
+*  - Joao Filipe Guiomar Artur, 2019217853
+*  - Sancho Amaral Simoes, 2019217590
+*
+* @date 22/05/2021
+*/
+
+//region dependencies
+
 #include "time.h"
 #include "errno.h"
 #include "stdlib.h"
 #include "numbers.h"
+
+//endregion dependencies
 
 int random_int(int min, int max) {
     return rand() % (max - min + 1) + min;
